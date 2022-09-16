cbc83ace689b9b565baab33064d87f82794be055
komiyamma <komiyama0629@gmail.com>
2022-09-16 22:19:22 +0900

    ワーストの時パフォーマンスがわるすぎたので、ほとんどのシーンでは少し落ちるが、やむなくmod使って対処

--- HmCustomLivePreview.src/hmJSStaticLib/ClearScript/hmJSStaticLib/hmCustomLivePreviewHidemaru.cs
+++ HmCustomLivePreview.src/hmJSStaticLib/ClearScript/hmJSStaticLib/hmCustomLivePreviewHidemaru.cs
@@ -55,6 +55,9 @@ public sealed partial class HmCustomLivePreviewDynamicLib
                 if (number > Int32.MaxValue)
                 {
                     number = number - 4294967296;
+                    number = number - Int32.MinValue;
+                    number = number % 4294967296;
+                    number = number + Int32.MinValue;
                 }
                 else
                 {
@@ -66,6 +69,9 @@ public sealed partial class HmCustomLivePreviewDynamicLib
                 if (number < Int32.MinValue)
                 {
                     number = number + 4294967296;
+                    number = number + Int32.MinValue;
+                    number = number % 4294967296;
+                    number = number - Int32.MinValue;
                 }
                 else
                 {
@@ -83,6 +89,7 @@ public sealed partial class HmCustomLivePreviewDynamicLib
             intvar = ret_number;
             return success;
         }
+
 
 
         private static bool IsDoubleNumeric(object value)
